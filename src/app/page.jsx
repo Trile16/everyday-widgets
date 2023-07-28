7a5d87facf1b7e8dd3e98a51621687853587c5ec
Tri <16trile@gmail.com>
2023-07-27 22:29:42 -0500

    added other widget images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,20 +4,39 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center p-16">
-      <div className="bg-gray rounded-xl p-8">
-        <h1 className="text-3xl/8 p-8 bg-indigo-500">Everyday Widgets!</h1>
-        <div>
+      <div className="bg-gradient-to-r from-blue to-purple rounded-xl p-8 w-5/6 max-w-xl">
+        <h1 className="text-3xl/8 p-8 text-center">Everyday Widgets!</h1>
+        <div className="flex justify-center items-center">
           <Link
             href="/weather"
-            className="flex flex-col items-center bg-blue w-fit rounded-xl"
+            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
+          >
+            <Image src="/clouds.png" width={125} height={125} alt="weather" />
+            <h2 className="text-xl/8">Weather</h2>
+          </Link>
+          <Link
+            href="/calculator"
+            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
           >
             <Image
-              src="/clouds.png"
+              src="/calculator.png"
               width={125}
               height={125}
-              alt="Picture of the author"
+              alt="calculator"
             />
-            <h2 className="text-xl/8">Weather</h2>
+            <h2 className="text-xl/8">Calculator</h2>
+          </Link>
+          <Link
+            href="/stopwatch"
+            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
+          >
+            <Image
+              src="/stopwatch.png"
+              width={125}
+              height={125}
+              alt="stopwatch"
+            />
+            <h2 className="text-xl/8">Stopwatch</h2>
           </Link>
         </div>
       </div>
