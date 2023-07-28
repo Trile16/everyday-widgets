33c6e8c80c3e394e1b6eeed494ff111b0e5e9235
Tri <16trile@gmail.com>
2023-07-27 23:43:25 -0500

    finished stopwatch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,43 +3,55 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center p-16">
-      <div className="bg-gradient-to-r from-blue to-purple rounded-xl p-8 w-5/6 max-w-xl">
-        <h1 className="text-3xl/8 p-8 text-center">Everyday Widgets!</h1>
-        <div className="flex justify-center items-center">
-          <Link
-            href="/weather"
-            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
-          >
-            <Image src="/clouds.png" width={125} height={125} alt="weather" />
-            <h2 className="text-xl/8">Weather</h2>
-          </Link>
-          <Link
-            href="/calculator"
-            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
-          >
-            <Image
-              src="/calculator.png"
-              width={125}
-              height={125}
-              alt="calculator"
-            />
-            <h2 className="text-xl/8">Calculator</h2>
-          </Link>
-          <Link
-            href="/stopwatch"
-            className="flex flex-col items-center bg-blue w-fit rounded-xl m-4"
-          >
-            <Image
-              src="/stopwatch.png"
-              width={125}
-              height={125}
-              alt="stopwatch"
-            />
-            <h2 className="text-xl/8">Stopwatch</h2>
-          </Link>
-        </div>
+    <div className="bg-gradient-to-r from-blue to-purple rounded-xl p-8 w-5/6 max-w-xl h-5/6 flex flex-col justify-between items-center">
+      <h1 className="text-3xl/8 p-8 text-center">Everyday Widgets!</h1>
+      <div className="flex flex-wrap justify-center items-center w-3/4">
+        <Link
+          href="/weather"
+          className="flex flex-col items-center bg-blue w-fit rounded-xl m-4 w-1/3"
+        >
+          <Image src="/clouds.png" width={125} height={125} alt="weather" />
+          <h2 className="text-xl/8">Weather</h2>
+        </Link>
+        <Link
+          href="/calculator"
+          className="flex flex-col items-center bg-blue w-fit rounded-xl m-4 w-1/3"
+        >
+          <Image
+            src="/calculator.png"
+            width={125}
+            height={125}
+            alt="calculator"
+          />
+          <h2 className="text-xl/8">Calculator</h2>
+        </Link>
+        <Link
+          href="/stopwatch"
+          className="flex flex-col items-center bg-blue w-fit rounded-xl m-4 w-1/3"
+        >
+          <Image
+            src="/stopwatch.png"
+            width={125}
+            height={125}
+            alt="stopwatch"
+            className="p-2"
+          />
+          <h2 className="text-xl/8">Stopwatch</h2>
+        </Link>
+        <Link
+          href="/clock"
+          className="flex flex-col items-center bg-blue w-fit rounded-xl m-4 w-1/3"
+        >
+          <Image
+            src="/clock.png"
+            width={125}
+            height={125}
+            alt="clock"
+            className="p-2"
+          />
+          <h2 className="text-xl/8">Clock</h2>
+        </Link>
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/stopwatch/page.jsx
+++ src/app/stopwatch/page.jsx
@@ -1,3 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
 export default function Stopwatch() {
-  return <>Stopwatch</>;
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (start) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!start) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [start]);
+
+  return (
+    <div className="bg-gradient-to-r from-blue to-purple rounded-xl p-8 w-5/6 max-w-xl h-5/6 flex flex-col justify-between items-center">
+      <h1 className="text-3xl/8 p-8 text-center">Stopwatch</h1>
+      <div className="bg-gray w-5/6 h-1/2 rounded-xl flex flex-col justify-center items-center">
+        <div className="text-5xl">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div>
+          <button className="m-4" onClick={() => setStart(true)}>
+            Start
+          </button>
+          <button className="m-4" onClick={() => setStart(false)}>
+            Stop
+          </button>
+          <button className="m-4" onClick={() => setTime(0)}>
+            Reset
+          </button>
+        </div>
+      </div>
+      <Link href="/">Home</Link>
+    </div>
+  );
 }
